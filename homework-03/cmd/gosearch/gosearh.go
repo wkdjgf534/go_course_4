c5ebe243dc35436c272da72d70926b1b9996dcd9
Eugene Makarikhin <wkdjgf534@users.noreply.github.com>
2023-07-09 20:16:58 +0200

    Task_03 Inverted index (#2)
    * Added previous project
    
    * Added index counter for documents
    
    * Added basic index package
    
    * Added index's method Add
    
    * Refactored index's functions
    
    * Updated the solution
    
    * Added first implementation for binary search
    
    * Fixed binary search
    
    * Added case-insenitive for sFlag
    
    * Renamed directories accroding to comment
    
    * Added more errors
    
    * Fixed grammar mistake
    
    * Got rid of constants
    
    * Removed unnecessary errors

---
+++ homework-03/cmd/gosearch/gosearh.go
@@ -0,0 +1,61 @@
+// Main package
+package main
+
+import (
+	"flag"
+	"fmt"
+	"go-course-4/homework-03/pkg/crawler"
+	"go-course-4/homework-03/pkg/crawler/spider"
+	"go-course-4/homework-03/pkg/index"
+	"strings"
+)
+
+func main() {
+	depth := 2
+	urls := []string{"https://golang.org", "https://www.practical-go-lessons.com/"}
+	sFlag := flag.String("s", "", "Use parameter -s and add a preferable key word (-s go)")
+	flag.Parse()
+
+	if len(*sFlag) == 0 {
+		flag.PrintDefaults()
+		return
+	}
+
+	var docs []crawler.Document
+	s := spider.New()
+	index := index.New()
+	counter := 0
+
+	for _, u := range urls {
+		links, err := s.Scan(u, depth)
+		if err != nil {
+			fmt.Printf("We got an error: %s\n", err)
+			continue
+		}
+
+		for _, l := range links {
+			l.ID = counter
+			docs = append(docs, l)
+			counter++
+		}
+	}
+
+	index.Add(&docs)
+
+	idx := index.Ids(strings.ToLower(*sFlag))
+
+	for _, i := range idx {
+		min, max := docs[0].ID, docs[len(docs)-1].ID
+		for min <= max {
+			mid := (min + max) / 2
+			if docs[mid].ID == i {
+				fmt.Println("found document id:", i, "URL:", docs[mid].URL)
+				break
+			} else if docs[mid].ID < i {
+				min = mid + 1
+			} else {
+				max = mid - 1
+			}
+		}
+	}
+}
