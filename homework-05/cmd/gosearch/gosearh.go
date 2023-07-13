5aa62ba6962ade6e549a282290c2a77de5b24c1b
Evgenii Makarikhin <contact@aboutslack.info>
2023-07-13 12:19:01 +0200

    Used flag.StringVar instead of flag.String, moved main function to top

--- homework-05/cmd/gosearch/gosearh.go
+++ homework-05/cmd/gosearch/gosearh.go
@@ -19,6 +19,60 @@ const (
 
 var urls = []string{"https://golang.org", "https://www.practical-go-lessons.com/"}
 
+func main() {
+	var f string
+	flag.StringVar(&f, "s", "", "Use parameter -s and add a preferable key word (-s go)")
+	flag.Parse()
+
+	if len(f) == 0 {
+		flag.PrintDefaults()
+		return
+	}
+
+	var docs []crawler.Document
+	s := spider.New()
+	ind := index.New()
+
+	docs, err := read(fName)
+	if err != nil {
+		fmt.Printf("Error: %s\nWe have to download new data again\n", err)
+	}
+
+	if len(docs) == 0 || err != nil {
+		for _, u := range urls {
+			links, err := s.Scan(u, depth)
+			if err != nil {
+				fmt.Printf("We got an error: %s\n", err)
+				continue
+			}
+			docs = append(docs, links...)
+		}
+		err := write(fName, &docs)
+		if err != nil {
+			fmt.Printf("Error: %s\nWe can not write new data to the file\n", err)
+		}
+	}
+
+	ind.Add(&docs)
+
+	idx := ind.Ids(strings.ToLower(f))
+
+	for _, i := range idx {
+		min, max := docs[0].ID, docs[len(docs)-1].ID
+		for min <= max {
+			mid := (min + max) / 2
+			if docs[mid].ID == i {
+				fmt.Println("found document id:", i, "URL:", docs[mid].URL)
+				break
+			} else if docs[mid].ID < i {
+				min = mid + 1
+			} else {
+				max = mid - 1
+			}
+		}
+	}
+}
+
 // read - Чтение из файла
 func read(name string) ([]crawler.Document, error) {
 	f, err := os.Open(name)
@@ -47,57 +101,4 @@ func write(name string, docs *[]crawler.Document) error {
 		return err
 	}
 	return nil
-}
-
-func main() {
-	sFlag := flag.String("s", "", "Use parameter -s and add a preferable key word (-s go)")
-	flag.Parse()
-
-	if len(*sFlag) == 0 {
-		flag.PrintDefaults()
-		return
-	}
-
-	var docs []crawler.Document
-	s := spider.New()
-	i := index.New()
-
-	docs, err := read(fName)
-	if err != nil {
-		fmt.Printf("Error: %s\nWe have to download new data again\n", err)
-	}
-
-	if len(docs) == 0 || err != nil {
-		for _, u := range urls {
-			links, err := s.Scan(u, depth)
-			if err != nil {
-				fmt.Printf("We got an error: %s\n", err)
-				continue
-			}
-			docs = append(docs, links...)
-		}
-		err := write(fName, &docs)
-		if err != nil {
-			fmt.Printf("Error: %s\nWe can not write new data to the file\n", err)
-		}
-	}
-
-	i.Add(&docs)
-
-	idx := i.Ids(strings.ToLower(*sFlag))
-
-	for _, i := range idx {
-		min, max := docs[0].ID, docs[len(docs)-1].ID
-		for min <= max {
-			mid := (min + max) / 2
-			if docs[mid].ID == i {
-				fmt.Println("found document id:", i, "URL:", docs[mid].URL)
-				break
-			} else if docs[mid].ID < i {
-				min = mid + 1
-			} else {
-				max = mid - 1
-			}
-		}
-	}
 }
