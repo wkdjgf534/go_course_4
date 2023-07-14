ab50a16e3c7526336670386bd38f42bd06dd02dd
Evgenii Makarikhin <contact@aboutslack.info>
2023-07-14 16:02:58 +0200

    Fixed minor mistake

--- homework-06/distance.go
+++ homework-06/distance.go
@@ -3,6 +3,6 @@ package distance
 import "math"
 
 // CalculateDistance - расчёт дистанции между 2 точками
-func CalculateDistance(x1, x2, y1, y2 float64) (distance float64) {
+func CalculateDistance(x1, x2, y1, y2 float64) float64 {
 	return math.Sqrt(math.Pow(math.Abs(x2)-math.Abs(x1), 2) + math.Pow(math.Abs(y2)-math.Abs(y1), 2))
 }
