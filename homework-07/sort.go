67a0e12e41fc7fc4f567bae4c4c60ddc2e78f7db
Evgenii Makarikhin <contact@aboutslack.info>
2023-07-15 12:52:41 +0200

    Added initial comment

---
+++ homework-07/sort.go
@@ -0,0 +1,9 @@
+package sort
+
+import "sort"
+
+func sortIntegers(nums []int) { return sort.Ints(nums) }
+
+func sortStrings(str []string) { return sort.Strings(str) }
+
+func sortFloats(nums []int) { return sort.Float64s(nums) }
