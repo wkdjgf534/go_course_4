ea3b52d6af80f61cc801c73aae7e9967182b445f
Evgenii Makarikhin <contact@aboutslack.info>
2023-07-16 21:21:30 +0200

    Added benchmarks for sort operations

--- homework-07/sort_test.go
+++ homework-07/sort_test.go
@@ -2,63 +2,57 @@ package sort
 
 import (
 	"reflect"
+	"sort"
 	"testing"
 )
 
-func Test_sortIntegers(t *testing.T) {
+func Test_sortInts(t *testing.T) {
 	nums := []int{3, 7, 1, 4, 2, 6, 5}
-	got := sortIntegers(nums)
+	sort.Ints(nums)
 	want := []int{1, 2, 3, 4, 5, 6, 7}
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %d, want %d", got, want)
+	if !reflect.DeepEqual(nums, want) {
+		t.Errorf("got %d, want %d", nums, want)
 	}
 	t.Log("When passed an unsorted slice of integers")
 }
 
 func Test_sortStrings(t *testing.T) {
-	type args struct {
-		strs []string
-	}
 	tests := []struct {
 		name string
-		args args
+		got  []string
 		want []string
 	}{
 		{
-			name: "when passed an unsortered slice of strings",
-			args: args{strs: []string{"Go", "Bravo", "Gopher", "Alpha", "Grin", "Delta"}},
+			name: "when passed an unsortered slice of strings #1",
+			got:  []string{"Go", "Bravo", "Gopher", "Alpha", "Grin", "Delta"},
 			want: []string{"Alpha", "Bravo", "Delta", "Go", "Gopher", "Grin"},
+		},
+		{
+			name: "when passed an unsortered slice of strings #2",
+			got:  []string{"5", "3", "4", "1", "2", "6"},
+			want: []string{"1", "2", "3", "4", "5", "6"},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := sortStrings(tt.args.strs); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("sortStrings() = %v, want %v", got, tt.want)
+			sort.Strings(tt.got)
+			if !reflect.DeepEqual(tt.got, tt.want) {
+				t.Errorf("sortStrings() = %v, want %v", tt.got, tt.want)
 			}
 		})
 	}
 }
 
-func Test_sortFloats(t *testing.T) {
-	type args struct {
-		nums []float64
+func Benchmark_sortInts(b *testing.B) {
+	s := []int{3, 7, 1, 4, 2, 6, 5}
+	for i := 0; i < b.N; i++ {
+		sort.Ints(s)
 	}
-	tests := []struct {
-		name string
-		args args
-		want []float64
-	}{
-		{
-			name: "when passed an unsortered slice of numbers",
-			args: args{nums: []float64{3.34, 0.12, 5.56, 1.23, 4.45}},
-			want: []float64{0.12, 1.23, 3.34, 4.45, 5.56},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := sortFloats(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("sortFloats() = %v, want %v", got, tt.want)
-			}
-		})
+}
+
+func Benchmark_sortFloat64s(b *testing.B) {
+	f := []float64{3.34, 0.12, 5.56, 1.23, 4.45}
+	for i := 0; i < b.N; i++ {
+		sort.Float64s(f)
 	}
 }
