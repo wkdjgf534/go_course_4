8f8fe2584f94040ccc20f58b0e0311ad00588f19
Evgenii Makarikhin <contact@aboutslack.info>
2023-07-24 01:36:31 +0200

    Added the solution for the second task

--- homework-09/maxage2/maxage2.go
+++ homework-09/maxage2/maxage2.go
@@ -1,5 +1,50 @@
 package main
 
-func MaxAge() {
+import (
+	"fmt"
+)
 
+type customer struct {
+	Age int
+}
+
+type employee struct {
+	Age int
+}
+
+// MaxAge - return the eldest person from a collection
+func MaxAge(p ...any) any {
+	var oldest any
+	maxAge := 0
+	for _, v := range p {
+		switch person := v.(type) {
+		case employee:
+			if person.Age > maxAge {
+				maxAge = person.Age
+				oldest = person
+			}
+
+		case customer:
+			if person.Age > maxAge {
+				maxAge = person.Age
+				oldest = person
+			}
+		default:
+			return 0
+		}
+	}
+	return oldest
+}
+
+func main() {
+	c1 := customer{Age: 66}
+	c2 := customer{Age: 33}
+
+	e1 := employee{Age: 77}
+	e2 := employee{Age: 45}
+
+	fmt.Println(MaxAge())
+	fmt.Println("The eldest person among customers", Oldest(c1, c2))
+	fmt.Println("The eldest person among employees", Oldest(e1, e2))
+	fmt.Println("The eldest person", MaxAge(c1, c2, e1, e2))
 }
