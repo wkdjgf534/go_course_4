542535efd23ee8b90c6b8d449c5542128f0da6d1
Evgenii Makarikhin <contact@aboutslack.info>
2023-07-30 02:16:42 +0200

    Rewrote function player

--- homework-10/cmd/main.go
+++ homework-10/cmd/main.go
@@ -7,48 +7,56 @@ import (
 )
 
 func main() {
-	min := 1.0
-	max := 100.0
-	minChance := max * 0.2
-	randNum := min + rand.Float64()*(max-min)
-
 	ch := make(chan string)
 
 	var wg sync.WaitGroup
 	wg.Add(2)
 
 	go func() {
-		player("Player 1", ch, minChance, randNum)
+		player("Player 1", ch)
 		wg.Done()
 	}()
 	go func() {
-		player("Player 2", ch, minChance, randNum)
+		player("Player 2", ch)
 		wg.Done()
 	}()
 
-	ch <- "start"
+	ch <- "begin"
 	wg.Wait()
 }
 
-func player(name string, table chan string, minChance float64, randNum float64) {
-	status := <-table
-	for {
-		if status == "start" {
-			fmt.Println("Someone lost", name)
+func player(name string, table chan string) {
+	min := 1.0
+	max := 100.0
+	point := 1
+	for t := range table {
+		minChance := max * 0.2
+		randNum := min + rand.Float64()*(max-min)
+
+		if t == "begin" {
+			fmt.Println("Player ", name, "lose!")
+			table <- "ping"
+		}
+
+		fmt.Println(randNum, minChance)
+		if randNum < minChance {
+			fmt.Printf("Player %s missed\n", name)
+			table <- "stop"
+		}
+
+		if t == "stop" {
+			close(table)
 			return
 		}
 
-		//table <- "stop"
-		//if status == "stop" {
-		//	fmt.Println("Someone lost", name)
-		//	//return
-		//}
+		if t == "ping" {
+			fmt.Println(t)
+			table <- "pong"
+		}
 
-		//close(table)
-		//if randNum < minChance {
-		//	fmt.Printf("Player %s missed\n", name)
-		//	close(table)
-		//	return
-		//}
+		if t == "pong" {
+			fmt.Println(t)
+			table <- "ping"
+		}
 	}
 }
