2cbfb8854dc46ed8c51222ec36ceb3879e56d781
Evgenii Makarikhin <contact@aboutslack.info>
2023-07-31 20:55:45 +0200

    Added initional commit

---
+++ homework-11/cmd/gosearch/gosearh.go
@@ -0,0 +1,12 @@
+// Main package
+package main
+
+import (
+	"go-course-4/homework-05/pkg/crawler"
+	"go-course-4/homework-05/pkg/crawler/spider"
+)
+
+func main() {
+	var docs []crawler.Document
+	s := spider.New()
+}
