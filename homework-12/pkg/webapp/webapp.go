f256963450b5c6edfdeffa9e7ba0d406b928ad45
Eugene Makarikhin <wkdjgf534@users.noreply.github.com>
2023-08-10 13:52:13 +0200

    Taks_12 Webapp (#11)
    * Added material from previous homework
    
    * Added simpe http server with 2 endpoints
    
    * Rewrote http server form std to  gorilla/mux
    
    * Added handler for docs and index
    
    * Refactored the solution
    
    * Uncommented index in maing
    
    * Merged changes from the previous task
    
    * Added wg for 2 servers
    
    * Added some unit tests
    
    * Removed unnecessary function
    
    * Reordered imports in unit test
    
    * Added api package with some functions
    
    * Removed api, incorrect task
    
    * Updated the solution
    
    * Added testify

---
+++ homework-12/pkg/webapp/webapp.go
@@ -0,0 +1,46 @@
+package webapp
+
+import (
+	"fmt"
+	"net/http"
+
+	"go-course-4/homework-12/pkg/index"
+)
+
+type WebApp struct {
+	index *index.Index
+}
+
+// New - конструктор для цуи сервера
+func New(ind *index.Index) WebApp {
+	return WebApp{index: ind}
+}
+
+// DocsHandler - функция возвращает список документов
+func (webapp *WebApp) DocsHandler(w http.ResponseWriter, r *http.Request) {
+	var pTags string
+	pTags = "Docs do not exist"
+
+	if len(webapp.index.Docs) != 0 {
+		pTags = ""
+
+		for _, doc := range webapp.index.Docs {
+			pTags += fmt.Sprintf("<p>%v - %v</p>", doc.ID, doc.Title)
+		}
+	}
+	fmt.Fprintf(w, "<html><body><div>%v</div></body></html>", pTags)
+}
+
+// IndexHandler - функция возвращает список тегов и массив id документов
+func (webapp *WebApp) IndexHandler(w http.ResponseWriter, r *http.Request) {
+	var pTags string
+	pTags = "Indexes do not exist"
+
+	if len(webapp.index.Docs) != 0 {
+		pTags = ""
+		for key, value := range webapp.index.Words {
+			pTags += fmt.Sprintf("<p>%v - %v</p>", key, value)
+		}
+	}
+	fmt.Fprintf(w, "<html><body><div>%v</div></body></html>", pTags)
+}
